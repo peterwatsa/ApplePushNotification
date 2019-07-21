107febbdba4bdc471a8a7b2da1e1f29b2ee78140
northpolelightyears@gmail.com <drumbase123*()>
2019-07-21 08:07:16 -0500

    first commit

---
+++ FirebaseStarterKit/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  FirebaseStarterKit
+//
+//  Created by Florian Marcu on 1/28/19.
+//  Copyright © 2019 Instamobile. All rights reserved.
+//
+
+import Firebase
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let pushManager = PushNotificationManager(userID: "currently_logged_in_user_id")
+        pushManager.registerForPushNotifications()
+
+        FirebaseApp.configure()
+
+        let sender = PushNotificationSender()
+        sender.sendPushNotification(to: "token", title: "Notification title", body: "Notification body")
+    
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseStarterKit' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseStarterKit
+  pod 'Firebase'
+  pod 'Firebase/Database'
+  pod 'Firebase/Messaging'
+  pod 'Firebase/Storage'
+  pod 'FirebaseFirestore'
+
+end
